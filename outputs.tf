929682d80c0e464279f0890a57b782fcd95f4e41
silvana castrillon <silvana.castrillon@outlook.com>
2023-10-12 21:47:40 +0000

    Restructure Root Module #21 Guardar Cambios

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "random_bucket_name_id"{
+  value = random_string.bucket_name.id   
+}
+
+output "random_bucket_name_result"{
+  value = random_string.bucket_name.result   
+}
+
+output "random_bucket_name"{
+  value = random_string.bucket_name.id   
+}
