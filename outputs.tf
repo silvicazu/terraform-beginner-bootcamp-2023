aee6cbcf49ed0e3acb6822b6032399b509f0b9bd
silvicazu <93725827+silvicazu@users.noreply.github.com>
2023-10-12 17:36:26 -0500

    Configuration Drift #23 bucket should no loger use remdon provider (#24)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "random_bucket_name_id"{
-  value = random_string.bucket_name.id   
-}
-
-output "random_bucket_name_result"{
-  value = random_string.bucket_name.result   
-}
-
-output "random_bucket_name"{
-  value = random_string.bucket_name.id   
+output "bucket_name" {
+  value = aws_s3_bucket.website_bucket.bucket
 }
