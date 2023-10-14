d604539e2c016c85b08e17cf1cf36fb1d8b54862
silvana castrillon <silvana.castrillon@outlook.com>
2023-10-14 00:47:29 +0000

    AWS Terrahouse Module #25 refactor s3 bucket into structure module

---
+++ modules/terrahouse.aws/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  #cloud {
+  #  organization = "ExamPro"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+
+}
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
